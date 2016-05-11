2d1e323a286a5942e5756652588fab719067716a
shanna <shanna6633@gmail.com>
2016-05-11 12:10:07 -0700

    First draft

---
+++ Five.hs
@@ -0,0 +1,97 @@
+-- CS583 Project: Five in a Row
+-- Xu Zheng (zhengxu) & Tien-Lung Chang (changti)
+
+module Five where
+
+data Cell = Black
+          | White
+          | Blank
+  deriving (Eq,Show)
+
+data Player = First
+            | Second
+  deriving (Eq,Show)
+
+data Board a = Board [[a]] deriving (Show)
+
+-- Initialize the board
+initBoard :: Int -> Board Cell
+initBoard x = Board (replicate x (replicate x Blank))
+
+-- Print the board
+-- If Blank -> *
+-- If Blakc -> O
+-- If White -> X
+showBoard :: Board Cell -> IO ()
+showBoard (Board (x:xs)) = 
+    let
+        colMark [] idx = putStrLn ""
+        colMark (y:ys) idx | idx < 10 = do putStr ((show idx) ++ "  ")
+                                           colMark ys (idx+1)
+                           | idx >= 10 = do putStr((show idx) ++ " ")
+                                            colMark ys (idx+1)
+
+        rowMark [] idx = return ()
+        rowMark (y:ys) idx | idx < 10 = do putStr ((show idx) ++ " ")
+                                           showCell y idx
+                                           rowMark ys (idx+1)
+                           | idx >= 10 = do putStr ((show idx) ++ "")
+                                            showCell y idx
+                                            rowMark ys (idx+1)
+
+        showCell [] idx    = print idx
+        showCell (y:ys) idx | y == Blank = do putStr " * "
+                                              showCell ys idx
+                            | y == Black = do putStr " O "
+                                              showCell ys idx
+                            | y == White = do putStr " X "
+                                              showCell ys idx
+    in
+        do
+            putStr "   "
+            colMark (x:xs) 1
+            rowMark (x:xs) 1
+            putStr "   "
+            colMark (x:xs) 1
+
+
+updateBoard :: Board Cell -> Int -> Int -> Player -> Board Cell
+updateBoard (Board x) col row player = 
+    if player == First then do
+      Board (take (row-1) x ++ [newRow] ++ drop row x)
+    else
+      Board (take (row-1) x ++ [newRow'] ++ drop row x)
+    where
+      newRow  = take (col-1) (x !! (row-1)) ++ [Black] ++ drop col (x !! (row-1))
+      newRow' = take (col-1) (x !! (row-1)) ++ [White] ++ drop col (x !! (row-1))
+
+
+gameLoop :: Board Cell -> Player -> IO ()
+gameLoop (Board x) player = 
+    do
+      showBoard (Board x)
+      currentPlayer player
+      putStr "Col: "
+      col <- getLine
+      putStr "Row: "
+      row <- getLine
+      gameLoop (updateBoard (Board x) (read col :: Int) (read row :: Int) player) (next player)
+    where
+    currentPlayer First = putStrLn "BLACK's turn: "
+    currentPlayer Second = putStrLn "White's turn: "
+    next First = Second
+    next Second = First
+
+
+-- Game begins here
+-- Players set the width and heigh of the board
+main :: IO ()
+main = do
+    putStr "Please input a number N to initialize a NxN board: "
+    n <- getLine
+    gameLoop (initBoard (read n::Int)) First
+
+
+
+
+
---
+++ README.txt
@@ -0,0 +1,64 @@
+CS583 Project: Five in a Row
+Xu Zheng (zhengxu) & Tien-Lung Chang (changti)
+
+1. Our goal is to implement a board game called "Five in a Row", which is
+   a two players game. The first player who make a coherently sequence of
+   five stones will win the game. The sequence can be vertically, horizontally
+   or diagonally.
+
+   Currently, we have defined the data type for our board and player. We
+   also have completed several functions, such as a function to generate
+   an initial board, a function to customize a board, a function to update
+   the current game status and a loop for run the game correctly.
+
+2. To run our project, just load "Five.hs" then input "main". There are
+   some example inputs and results as follows:
+
+   >> ghci Five.hs
+   >> *Five> main
+   >> Please input a number N to initialize a NxN board: 10
+   >>    1  2  3  4  5  6  7  8  9  10
+   >> 1  *  *  *  *  *  *  *  *  *  * 1
+   >> 2  *  *  *  *  *  *  *  *  *  * 2
+   >> 3  *  *  *  *  *  *  *  *  *  * 3
+   >> 4  *  *  *  *  *  *  *  *  *  * 4
+   >> 5  *  *  *  *  *  *  *  *  *  * 5
+   >> 6  *  *  *  *  *  *  *  *  *  * 6
+   >> 7  *  *  *  *  *  *  *  *  *  * 7
+   >> 8  *  *  *  *  *  *  *  *  *  * 8
+   >> 9  *  *  *  *  *  *  *  *  *  * 9
+   >> 10 *  *  *  *  *  *  *  *  *  * 10
+   >> BLACK's turn:
+   >> Col: 3
+   >> Row: 3
+   >>    1  2  3  4  5  6  7  8  9  10
+   >> 1  *  *  *  *  *  *  *  *  *  * 1
+   >> 2  *  *  *  *  *  *  *  *  *  * 2
+   >> 3  *  *  O  *  *  *  *  *  *  * 3
+   >> 4  *  *  *  *  *  *  *  *  *  * 4
+   >> 5  *  *  *  *  *  *  *  *  *  * 5
+   >> 6  *  *  *  *  *  *  *  *  *  * 6
+   >> 7  *  *  *  *  *  *  *  *  *  * 7
+   >> 8  *  *  *  *  *  *  *  *  *  * 8
+   >> 9  *  *  *  *  *  *  *  *  *  * 9
+   >> 10 *  *  *  *  *  *  *  *  *  * 10
+   >> WHITE's turn:
+   >> Col: 4
+   >> Row: 4
+   >>    1  2  3  4  5  6  7  8  9  10
+   >> 1  *  *  *  *  *  *  *  *  *  * 1
+   >> 2  *  *  *  *  *  *  *  *  *  * 2
+   >> 3  *  *  O  *  *  *  *  *  *  * 3
+   >> 4  *  *  *  X  *  *  *  *  *  * 4
+   >> 5  *  *  *  *  *  *  *  *  *  * 5
+   >> 6  *  *  *  *  *  *  *  *  *  * 6
+   >> 7  *  *  *  *  *  *  *  *  *  * 7
+   >> 8  *  *  *  *  *  *  *  *  *  * 8
+   >> 9  *  *  *  *  *  *  *  *  *  * 9
+   >> 10 *  *  *  *  *  *  *  *  *  * 10
+   >> BLACK's turn:
+   >> Col:
+
+3. Q1: What kind of ability do you think a successful A.I. must have?
+   Q2: How to apply monad to our project?
+
